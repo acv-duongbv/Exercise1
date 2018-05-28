0fb1b1ac77cc701414e71a098f757539ab9ea79a
bvduong <duongbv@atmarkcafe.org>
2018-05-28 21:29:21 +0700

    Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,31 @@
 class Article < ApplicationRecord
   has_many :images
   has_many :texts
+
+  def get_text_image_instance
+    img = Image.eager_load(:articles).where(article: self)
+    txt = Text.eager_load(:articles).where(article: self)
+    (img + text).sort_by{|i| i.arrange}
+  end
+
+  def getContent
+    self.get_text_image_instance.map{|i| i.class.name == 'Image' ? i.url : i.sentence}
+  end
+
+  def published
+    !self.new_record
+  end
+
+  def finished
+    published || self.create_at == DateTime.now
+  end
+
+  def addLike
+    self.liked++
+    self.save
+  end
+
+  def getLike
+    self.liked
+  end
 end
